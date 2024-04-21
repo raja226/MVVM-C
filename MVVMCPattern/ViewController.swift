671818bff7319563eb3dba4a967389bf7f4903a9
raja226 <iosrajasekhar@gmail.com>
2024-04-21 12:18:17 +0530

    Initial Commit

---
+++ MVVMCPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMCPattern
+//
+//  Created by Administrator on 21/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
